325d9fe2eb6284eb4178fba449a3627ab891545c
MatthewHeinrich <mheinrich95@gmail.com>
2021-03-26 16:38:36 -0500

    getting the data and rendering on DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+
+  const [ gallery, setGallery ] = useState([]);
+
+  useEffect( () =>{
+    getGallery();
+  },[])
+
+  let getGallery = () => {
+    axios.get( '/gallery' ).then( ( response ) =>{
+      console.log('back from GET:', response.data )
+      setGallery( response.data );
+    }).catch( (err)=>{
+      console.log(err);
+      alert( 'Sorry Please Try Again' );
+    })
+  }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <ul>
+          {gallery.map( (item, index)=> <li key={index}> <img src={item.path} ></img> {item.description} {item.likes} </li> )}
+        </ul>
       </div>
     );
 }
